ab5d21fd5aac476eb72f83941265c20ef534056b
YuriRocha <yurid.rocha@gmail.com>
2017-08-07 13:11:28 -0300

    adding SelectHandler

--- src/SelectHandler.js
+++ src/SelectHandler.js
@@ -1,5 +1,30 @@
 class SelectHandler{
-  function handle(sceneFile, campaignState, select_id){
+  function handle(scene, campaignState, targetID){
+    var response = "";
+    var select_options = scene.selects;
+    var handler;
+    var select = select_options.filter(function(opt){
+      return opt.id === targetID;
+    });
+    response += select.description + "\n\n";
+    response += select.options.forEach(function(obj, index){
+      	response += index + ": " + obj.description + "\n\n"
+    });
+    var target = chooseOptions(id_option);
 
+    function chooseOptions(id_option){
+      return select.options.filter(function(opt){
+        if(opt.id === id_option){
+          return opt.targetID;
+        }
+      });
+    }
+
+    if(target.result === undefined){
+      //do nothing
+    }
+
+    handler = new HandlerFactory().createHandler(scene.defaultActionChain[campaignState.nextSceneAction].targetType);
+    response += handler.handle(scene, campaignState, target);
   }
 }
